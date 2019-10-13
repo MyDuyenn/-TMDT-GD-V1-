547776f4b1494bfa809770f071904625fad3e509
Duyên Nguyễn <mduyen0212@gmail.com>
2019-10-13 12:32:01 +0700

    Giao dien V1

---
+++ MyBakery/app/src/main/java/com/example/myapplication/ButterCake.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButterCake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_butter_cake);
+    }
+}
---
+++ MyBakery/app/src/main/java/com/example/myapplication/CheckOutAddressItem.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckOutAddressItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out_address_item);
+    }
+}
